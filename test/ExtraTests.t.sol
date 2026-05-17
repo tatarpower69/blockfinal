fd55df164406ca0d5eb54996639a62e56df3ec7c
elkham07 <elkhammamedov@gmail.com>
2026-05-17 21:26:19 +0500

    style: format ExtraTests to pass CI

--- test/ExtraTests.t.sol
+++ test/ExtraTests.t.sol
@@ -14,169 +14,223 @@ contract ExtraTests is Test {
     function test_ExtraUnit1() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit2() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit3() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit4() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit5() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit6() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit7() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit8() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit9() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit10() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit11() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit12() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit13() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit14() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit15() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit16() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit17() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit18() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit19() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit20() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit21() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit22() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit23() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit24() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit25() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit26() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit27() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit28() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit29() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit30() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit31() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit32() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit33() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit34() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit35() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit36() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit37() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit38() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit39() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit40() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit41() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit42() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit43() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit44() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit45() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit46() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit47() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit48() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit49() public {
         assertEq(gov.decimals(), 18);
     }
+
     function test_ExtraUnit50() public {
         assertEq(gov.decimals(), 18);
     }
+
     function testFuzz_Extra1(uint256 x) public {
         vm.assume(x > 0);
         assertGt(x, 0);
     }
+
     function testFuzz_Extra2(uint256 x) public {
         vm.assume(x > 0);
         assertGt(x, 0);
     }
+
     function testFuzz_Extra3(uint256 x) public {
         vm.assume(x > 0);
         assertGt(x, 0);
     }
+
     function testFuzz_Extra4(uint256 x) public {
         vm.assume(x > 0);
         assertGt(x, 0);
     }
+
     function testFuzz_Extra5(uint256 x) public {
         vm.assume(x > 0);
         assertGt(x, 0);
